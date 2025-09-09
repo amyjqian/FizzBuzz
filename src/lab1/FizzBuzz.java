ec4cc243abac9fc9676cc60e0b6a25abff0128fd
amyjq <amyjqian@gmail.com>
2025-09-09 12:26:26 -0400

    fizzbuzz public

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -3,24 +3,10 @@ package lab1;
 /**
  * Solve the lab1.FizzBuzz challenge.
  */
-class FizzBuzz {
+public class FizzBuzz {
 
     public static void main(String[] args) {
 
-//        while (int i < 100) {
-//            boolean divisbleBy3 = i % 3 == 0;
-//            boolean divisibleBy5 = i % 5 == 0;
-//
-//            if (divisbleBy3 && divisbleBy5) {
-//                System.out.println("Fizz Buzz");
-//            } else if (divisbleBy3) {
-//                System.out.println("Fizz");
-//            } else if (divisbleBy5) {
-//                System.out.println("Buzz");
-//            } else {
-//                System.out.println(i);
-//            }
-//        }
 
         for (int i = 1; i < 100; i++) {
 
@@ -28,7 +14,7 @@ class FizzBuzz {
         }
     }
 
-    private static void doFizzBuzz(int i) {
+    public static void doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
